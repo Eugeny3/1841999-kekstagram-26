a9120fb97e364625b25d8413d23d588c365cac4e
Eugeny3 <hello31323334@gmail.com>
2022-07-06 23:56:01 +0500

    Отрисуй меня полностью часть 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import {createPhotoDescription, PHOTO_DESCRIPTION_COUNT} from './data.js';
+import {userComments} from './pictures.js';
+
+const commentsList = createPhotoDescription();
+userComments(commentsList);
 
 createPhotoDescription(PHOTO_DESCRIPTION_COUNT);
